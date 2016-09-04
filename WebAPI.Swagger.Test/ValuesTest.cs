004fb25f8003da2461e49661fcc40e754b4bffbc
PatrickJones <hiramdeveloper@gmail.com>
2016-09-03 17:04:23 -0700

    Updated unit test

---
+++ WebAPI.Swagger.Test/ValuesTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebAPI.Swagger.Controllers;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+
+namespace WebAPI.Swagger.Test
+{
+    /// <summary>
+    /// Unit test for values controller
+    /// </summary>
+    [TestClass]
+    public class ValuesTest
+    {
+        [TestMethod]
+        public void GetValueStrings()
+        {
+            //Arrange
+            var controller = new ValuesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            Array ar;
+            Assert.IsTrue(result.TryGetContentValue<Array>(out ar));
+            Assert.AreEqual("value1", ar.GetValue(0));
+            Assert.AreEqual("value2", ar.GetValue(1));
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetInputValue()
+        {
+            //Arrange
+            var controller = new ValuesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var param = 5;
+
+            //Act
+            var result = controller.Get(param);
+
+            //Assert
+            string str;
+            Assert.IsTrue(result.TryGetContentValue<string>(out str));
+            Assert.AreEqual($"value:{param}", str);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void PostValue()
+        {
+            //Arrange
+            var controller = new ValuesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var param = "value test";
+
+            //Act
+            var result = controller.Post(param);
+
+            //Assert
+            string str;
+            Assert.IsTrue(result.TryGetContentValue<string>(out str));
+            Assert.AreEqual(param, str);
+            Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
+
+        }
+
+        [TestMethod]
+        public void PutValue()
+        {
+            //Arrange
+            var controller = new ValuesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var idParam = 3;
+            var param = "value test";
+
+            //Act
+            var result = controller.Put(idParam, param);
+
+            //Assert
+            int id;
+            Assert.IsTrue(result.TryGetContentValue<int>(out id));
+            Assert.AreEqual(idParam, id);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void DeleteValue()
+        {
+            //Arrange
+            var controller = new ValuesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+            var idParam = 7;
+
+            //Act
+            var result = controller.Delete(idParam);
+
+            //Assert
+            int id;
+            Assert.IsTrue(result.TryGetContentValue<int>(out id));
+            Assert.AreEqual(idParam, id);
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
--- WebAPI.Swagger/Controllers/ValuesController.cs
+++ WebAPI.Swagger/Controllers/ValuesController.cs
@@ -12,30 +12,34 @@ namespace WebAPI.Swagger.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value1", "value2" };
+            var strs = new string[] { "value1", "value2" };
+            return Request.CreateResponse<Array>(HttpStatusCode.OK, strs);
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return "value";
+            return Request.CreateResponse<string>(HttpStatusCode.OK, $"value:{id}");
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody]string value)
         {
+            return Request.CreateResponse<string>(HttpStatusCode.Created, value);
         }
-
+        
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
+        public HttpResponseMessage Put(int id, [FromBody]string value)
         {
+            return Request.CreateResponse<int>(HttpStatusCode.OK, id);
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            return Request.CreateResponse<int>(HttpStatusCode.OK, id);
         }
     }
 }
