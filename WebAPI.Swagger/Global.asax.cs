e6dd4cf0d25a8a92ff298463b8a93af660459c54
PatrickJones <hiramdeveloper@gmail.com>
2016-09-04 21:54:56 -0700

    Added configuration to prevent self referencing loops when serializing into json

--- WebAPI.Swagger/Global.asax.cs
+++ WebAPI.Swagger/Global.asax.cs
@@ -15,6 +15,7 @@ namespace WebAPI.Swagger
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
